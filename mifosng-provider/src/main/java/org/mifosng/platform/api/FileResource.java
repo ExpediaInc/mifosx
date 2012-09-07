9b851ab3816eee7380294fcb3b60255223092622
Magesh Chandramouli <magesh@gmail.com>
2012-09-07 16:23:08 -0700

    adding a property named document.root for doc location. Needs to be revisited later

--- mifosng-provider/src/main/java/org/mifosng/platform/api/FileResource.java
+++ mifosng-provider/src/main/java/org/mifosng/platform/api/FileResource.java
@@ -6,23 +6,22 @@ package org.mifosng.platform.api;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+
 import org.apache.commons.io.FileUtils;
-import org.mifosng.platform.api.data.FileUploadResult;
-import org.mifosng.platform.api.data.ListFilesResult;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
+
+import org.mifosng.platform.api.data.FileUploadResult;
+import org.mifosng.platform.api.data.ListFilesResult;
 
 /**
  * @author sanpatel
@@ -31,10 +30,14 @@ import com.sun.jersey.multipart.FormDataParam;
 @Path("/file")
 @Component
 public class FileResource {
-	
+
+    @Value("${document.root}")
 	private String rootDir;
 
-	@Autowired
+	public FileResource()
+    {
+    }
+
 	public FileResource(String rootDir) {
 		this.rootDir = rootDir;
 	}
